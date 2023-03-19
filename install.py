6b4501ef0903e3b46992d67db87efe16b854125e
青龍聖者@bdsqlsz <865105819@qq.com>
2023-03-19 23:03:38 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("rem_bg"):
-    launch.run_pip("install rem_bg", "requirements for clothseg")
+    launch.run_pip("install rembg", "requirements for clothseg")
